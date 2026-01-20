bdd709b2532f1f65061105598aec033ceb6429d0
Alexandru Popa <8577646+popa-alex@users.noreply.github.com>
2026-01-20 18:01:46 +0200

    added functions to parse report

--- app/main.py
+++ app/main.py
@@ -1,13 +1,28 @@
+def get_property(prop: str) -> str | bool:
+    if prop == "line_number":
+        return "line"
+    if prop == "column_number":
+        return "column"
+    if prop == "text":
+        return "message"
+    if prop == "code":
+        return "name"
+    return "source"
+
+
 def format_linter_error(error: dict) -> dict:
-    # write your code here
-    pass
+    return {**{get_property(key): value for key,
+            value in error.items() if get_property(key)},
+            "source": "flake8"}
 
 
 def format_single_linter_file(file_path: str, errors: list) -> dict:
-    # write your code here
-    pass
+    return {
+        "errors": [format_linter_error(error) for error in errors],
+        "path": file_path,
+        "status": "failed" if len(errors) else "passed"}
 
 
 def format_linter_report(linter_report: dict) -> list:
-    # write your code here
-    pass
+    return [format_single_linter_file(key, value) for key,
+            value in linter_report.items()]
