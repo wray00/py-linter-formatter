60b3ceaa12e014432473bb8f15f49175c90162da
Alexandru Popa <8577646+popa-alex@users.noreply.github.com>
2026-01-21 10:35:57 +0200

    addresed PR comments

--- app/main.py
+++ app/main.py
@@ -1,18 +1,16 @@
-def get_property(prop: str) -> str | bool:
-    if prop == "line_number":
-        return "line"
-    if prop == "column_number":
-        return "column"
-    if prop == "text":
-        return "message"
-    if prop == "code":
-        return "name"
-    return "source"
+error_dict = {
+    "line_number": "line",
+    "column_number": "column",
+    "text": "message",
+    "code": "name",
+    "filename": "source",
+    "physical_line": "source"
+}
 
 
 def format_linter_error(error: dict) -> dict:
-    return {**{get_property(key): value for key,
-            value in error.items() if get_property(key)},
+    return {**{error_dict[property_key]: property_value for property_key,
+            property_value in error.items()},
             "source": "flake8"}
 
 
@@ -24,5 +22,5 @@ def format_single_linter_file(file_path: str, errors: list) -> dict:
 
 
 def format_linter_report(linter_report: dict) -> list:
-    return [format_single_linter_file(key, value) for key,
-            value in linter_report.items()]
+    return [format_single_linter_file(file_path, errors) for file_path,
+            errors in linter_report.items()]
